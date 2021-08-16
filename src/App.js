376e3efc6f3696415a4472672b6a7c1d9ca85e58
Elisabeth Asbæk Jeppesen <elisabeth.asbaek@gmail.com>
2021-08-16 11:05:08 +0200

    kjfdb

--- src/App.js
+++ src/App.js
@@ -8,9 +8,25 @@ import SingleView from "./pages/SingleView";
 export default function App() {
     var searchState = useState([]);
 
+    Notification.requestPermission(function(status){
+        console.log("Notification permission status: ", status);
+    });
+
+    function displayNotification(){
+        if(Notification.permission === "granted"){
+            navigator.serviceWorker.getRegistration()
+                .then(function(reg){
+                    reg.showNotification("Permission granted");
+                    console.log(reg);
+                })
+        }
+    }
+
     return (
         <SearchContext.Provider value={searchState}>
             <div className="App">
+                <button onClick={() => displayNotification()}>Notify me!</button>
+
                 <Router>
                     <Results default path="/" />
                     <SingleView path="/movie/:id" />
